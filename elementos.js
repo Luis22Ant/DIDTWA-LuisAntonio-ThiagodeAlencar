935ab938aa72366bbc484d16b638bed891a3cc64
Luis22Ant <101722544+Luis22Ant@users.noreply.github.com>
2023-06-29 14:17:47 -0300

    Add files via upload

---
+++ elementos.js
@@ -0,0 +1,32 @@
+    export {
+        buttonPesquisar,
+        buttonMode,
+        paragrafo,
+        buttonProximo,
+        buttonAnterior,
+        input,
+        nomeJogo,
+        imagem,
+        genero,
+        classificacao,
+        desenvolvedora,
+        descricao,
+        body,
+        footer,
+      };
+
+
+const body = document.querySelector('body')
+const buttonPesquisar = document.getElementById('buttonPesquisar');
+const buttonMode = document.getElementById('modoTela');
+const paragrafo = document.querySelectorAll('p');
+const buttonProximo = document.getElementById('buttonProximo');
+const buttonAnterior = document.getElementById('buttonAnterior');
+const input = document.getElementById('textoJogo');
+const nomeJogo = document.querySelector('#Cabecalho .nomeGame')
+const imagem = document.getElementById('gameImagem');
+const genero = document.querySelector('#informacoes .generoJogo');
+const classificacao = document.querySelector('#informacoes .classificacao');
+const desenvolvedora = document.querySelector('#informacoes .desenvolvedora');
+const descricao = document.querySelector('#informacoes .descricao');
+const footer = document.querySelector('footer');
